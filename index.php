cc46c7b8d345d50977d514706e23106cc5e19a42
KapoushianA <armen.goml@yahoo.com>
2015-05-19 11:13:27 -0700

    here is our bio

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ and open the template in the editor.
     </head>
     <body background="http://www.awshdwallpapers.com/wp-content/uploads/2014/06/awesome-plain-purple-wallpaper.jpg">
         <div class="jumbotron">
-            <h1>HI THERE</h1>
+            <h1>Street Kombat Brawl X</h1>
             <p></p>
             <div class="row">
                 <div class="col-sm-6">
@@ -32,6 +32,13 @@ and open the template in the editor.
                 </div>
             </div>
         </div>
+         <p> </p>
+                        <div class="col-xs-3"> <img class="img-responsive img-thumbnail" src="http://0.media.dorkly.cvcdn.com/14/77/135ebfa57ad5e4a10967638c1e1211ca-villager.jpg"></div>
+                        <div class="col-xs-9"><h2 style="color: white;">Armen and Kent</h2>
+                            <p style="color: white;">we are Freshman at <a href="http://phs.pasadenausd.org/">Pasadena High School</a>.
+                                <br/> Armen is the smart one.<br>
+                            <---- Kent.</p>
+                            <p></p> </div>
         <?php
         // put your code here
         ?>
